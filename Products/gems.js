3c56ee48074fd4eca25995327e58467e1ce11d17
Austin Summerlin <summerlin.austin@gmail.com>
2021-03-30 15:50:07 -0700

    add storefront

---
+++ Products/gems.js
@@ -0,0 +1,9 @@
+import { gems } from '../gems-data.js';
+import { makeGemLi } from '../utils.js';
+
+const ul = document.querySelector('.gemList');
+
+for (let gem of gems) {
+    const li = makeGemLi(gem);
+    ul.append(li);
+}
--- utils.js
+++ utils.js
@@ -1,3 +1,38 @@
 export function makeGemLi(gems) {
     const li = document.createElement('li');
+
+    li.classList.add('gemstones');
+    li.style.color = gems.description;
+
+    const divName = document.createElement('div');
+
+    divName.classList.add('name');
+    divName.textContent = gems.name;
+
+    const image = document.createElement('img');
+
+    image.src = gems.image;
+
+    const divDescription = document.createElement('div');
+
+    divDescription.classList.add('description');
+    divDescription.textContent = gems.description;
+    divDescription.color = gems.description;
+
+    const divCategory = document.createElement('div');
+
+    divCategory.classList.add('category');
+    divCategory.textContent = gems.category;
+
+    const divPrice = document.createElement('div');
+
+    divPrice.textContent = gems.price;
+
+    const button = document.createElement('button');
+
+    button.textContent = 'Purchase';
+
+    li.append(divName, image, divDescription, divCategory, divPrice, button);
+
+    return li;
 }
